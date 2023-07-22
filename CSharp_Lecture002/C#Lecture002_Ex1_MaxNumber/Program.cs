f13f5c39ab40b407bd4e347c056758ad4056d93b
DemchukDS <demchuk.btg@gmail.com>
2023-07-22 09:57:47 +0300

    Лекция 2, занятие 1

---
+++ CSharp_Lecture002/C#Lecture002_Ex1_MaxNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿//Найти максимум из 9 чисел
+
+Console.Clear();
+Console.Writeline("Enter the first number: ");
+double firstNumber = Convert.ToInt32(Console.Readline); Console.Writeline(firstNumber);
+Console.Writeline("Enter the second number: ");
+
+Console.Writeline("Enter the third number: ");
+
+Console.Writeline("Enter the fourth number: ");
+
+Console.Writeline("Enter the fifth number: ");
+
+Console.Writeline("Enter the sixth number: ");
+
+Console.Writeline("Enter the seventh number: ");
+
+Console.Writeline("Enter the eighth number: ");
+
+Console.Writeline("Enter the ninth number: ");
