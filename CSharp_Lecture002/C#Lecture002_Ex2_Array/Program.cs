63eeafedca500be4568d232aec11581bc4e4bdfa
DemchukDS <demchuk.btg@gmail.com>
2023-07-22 10:50:53 +0300

    Lecture 2, ex 2, Array

---
+++ CSharp_Lecture002/C#Lecture002_Ex2_Array/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+double Max (double arg1, double arg2, double arg3) // FUNCTION
+{
+    double result = arg1;
+    if (arg2 > result) result = arg2; // сокращение условия if в одну строку
+    if (arg3 > result) result = arg3;
+    return result;
+}
+//                  0  1   2  3   4   5   6   7   8
+double [] array = {10, 2, 34, 4, 45, 67, 74, 18, 39};
+
+double max = Max(Max(array[0], array[1], array[2]),
+                Max(array[3], array[4], array[5]),
+                Max(array[6], array[7], array[8]));
+
+Console.WriteLine(max);
