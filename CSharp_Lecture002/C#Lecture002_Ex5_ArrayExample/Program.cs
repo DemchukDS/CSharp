beec9531c779b6e18aff910a9d0992e9b13b0f78
DemchukDS <demchuk.btg@gmail.com>
2023-07-22 11:58:02 +0300

    Lecture 2, ex5, correct

---
+++ CSharp_Lecture002/C#Lecture002_Ex5_ArrayExample/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+void FillArray(int[] collection) // метод заполнения массива
+{
+    for (int i = 0; i < collection.Length; i++)
+    {
+        collection[i] = new Random().Next(1, 10);
+    }
+}
+
+void PrintArray(int[] print)
+{
+    for (int j = 0; j < print.Length; j++)
+    {
+        Console.Write(print[j]);
+        Console.Write(" ");
+    }
+}
+
+int[] randomArray = new int[10];
+
+FillArray(randomArray);
+PrintArray(randomArray);
