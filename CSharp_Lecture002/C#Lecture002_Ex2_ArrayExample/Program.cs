939141842ef5569f508c0e201be62a1bc9b8bbee
DemchukDS <demchuk.btg@gmail.com>
2023-07-22 11:30:55 +0300

    Lecture 2, ex2, ArrayTest

--- CSharp_Lecture002/C#Lecture002_Ex2_ArrayExample/Program.cs
+++ CSharp_Lecture002/C#Lecture002_Ex2_ArrayExample/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿Console.Clear();
+
+int[] array = {87, 2, 31, 43, 15};
+Console.WriteLine("Enter the number to be copared with the elements of the array: ");
+int find = Convert.ToInt32(Console.ReadLine());
+for (int index = 0; index < array.Length; index++)
+{
+    if (array[index] == find) 
+    {
+        Console.Write("The position number in array enterred symbol is - ");
+        Console.WriteLine(index);
+        break;
+    }
+}
