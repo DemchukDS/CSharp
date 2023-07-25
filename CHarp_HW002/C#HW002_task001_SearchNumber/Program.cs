e1373770d008aa21333cf47b2b2cca8eb8ceed9e
DemchukDS <demchuk.btg@gmail.com>
2023-07-25 23:06:57 +0300

    Seminar 2, HW, ex 1

---
+++ CHarp_HW002/C#HW002_task001_SearchNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿//  Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+Console.Write("Enter three-diginit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 1000 && number > 99)
+{
+    int a = number % 100;
+    int solution = a / 10;
+    Console.WriteLine($"Вторая цифра введённого числа: {solution}");
+}
+else
+{
+    Console.WriteLine($"Число не трёхзначное");
+}
