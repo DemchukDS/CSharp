8ef079611c8c39042e3ea6ab0497ec1ba718d749
DemchukDS <demchuk.btg@gmail.com>
2023-07-25 23:47:28 +0300

    Seminar 2, HW, ex 2

---
+++ CHarp_HW002/C#HW002_task002_SearchNumber3/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6 */
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+while (number > 1000)
+{
+    number = number / 10;
+}
+
+if (number > 99)
+{
+    number = number % 10;
+    Console.WriteLine($"Третья цифра введенного числа - {number}");
+}
+else if (number < 99)
+{
+    Console.WriteLine($"Третей цифры в введенном числе нет!");
+}
