a4588d00e992988881f3a40fa7a2cf27220b79cf
DemchukDS <demchuk.btg@gmail.com>
2023-07-25 21:24:49 +0300

    Seminar2, ex 2

--- CSharp_Seminar002/C#Seminar002_task002_TwoOutOfThree/Program.cs
+++ CSharp_Seminar002/C#Seminar002_task002_TwoOutOfThree/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Random rand = new Random ();
+int number = rand.Next(100, 1000); // int number = new Random().Next(10, 100);
+
+int a = number / 100;
+int b = number % 10;
+
+Console.WriteLine($"The number is {number}, {a} + {b}");
