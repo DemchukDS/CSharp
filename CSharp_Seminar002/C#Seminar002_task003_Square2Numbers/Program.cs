2254ce272232f540018e3a692bf6fc4b65c0a1b4
DemchukDS <demchuk.btg@gmail.com>
2023-07-25 21:47:38 +0300

    Seminar2, ex 3

--- CSharp_Seminar002/C#Seminar002_task003_Square2Numbers/Program.cs
+++ CSharp_Seminar002/C#Seminar002_task003_Square2Numbers/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на ввод два числа и проверяет, является ли одно число квадратом другого.
+
+// 1) ввод с клавиатуры 2 чисел
+// 2) возведение первого числа в квадрат.
+// 3) сравнение полученного числа со всторым.
+// 4) вывод ответа.
+
+Console.Clear();
+
+Console.Write("Enter the first number: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the second number: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+int square = firstNumber * firstNumber;
+
+if (square == secondNumber)
+{
+    Console.WriteLine($"{firstNumber} entered in the square to {secondNumber}!");
+}
+else
+{
+    Console.WriteLine($"{firstNumber} entered in the square is not equal to {secondNumber}!");
+}
