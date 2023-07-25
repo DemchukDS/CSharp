6bcfd4b8d24c22ff73115f3571dfed5c34050903
DemchukDS <demchuk.btg@gmail.com>
2023-07-25 21:48:44 +0300

    Seminar2, ex 2

--- CSharp_Seminar002/C#Seminar002_task002_TwoOutOfThree/Program.cs
+++ CSharp_Seminar002/C#Seminar002_task002_TwoOutOfThree/Program.cs
@@ -5,4 +5,4 @@ int number = rand.Next(100, 1000); // int number = new Random().Next(10, 100);
 int a = number / 100;
 int b = number % 10;
 
-Console.WriteLine($"The number is {number}, {a} + {b}");
+Console.WriteLine($"The number is {number}, {a}{b}");
