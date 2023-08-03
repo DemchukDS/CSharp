1a03d671bc54fbfad04642b14a557a413ed73746
DemchukDS <demchuk.btg@gmail.com>
2023-08-03 23:22:20 +0300

    Sem3, ex3

---
+++ CSharp_Seminar003/C#Seminar003_task003_Square/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая на вход принимает число (N) и выдает таблицу квадратов чисел от 1 до N.
+
+Console.Clear();
+
+int Promo(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+void Square(int number)
+{
+    for (int i = 0; i < number; i++)
+    {
+        int volue = i * i;
+        Console.Write($"{volue}, ");
+    }
+    Console.Write($"{number * number}");
+}
+
+int N = Promo("Введите число: ");
+Square(N);
+
+Console.ReadKey();
