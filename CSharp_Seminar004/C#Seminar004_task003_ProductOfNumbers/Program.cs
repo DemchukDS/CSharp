6e1dd1345107a29af28f7927d0a98b9992b80895
DemchukDS <demchuk.btg@gmail.com>
2023-08-11 11:15:10 +0300

    1

---
+++ CSharp_Seminar004/C#Seminar004_task003_ProductOfNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+
+int EnterNumber(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+bool Null(int number)
+{
+    if (number <= 0)
+    {
+        Value(EnterNumber("The number N must not equal 0! Try enter a new number: "));
+        return true;
+    }
+    return false;
+}
+int Value(int number)
+{
+    int prod = 1;
+    for (int i = 1; i < number + 1; i++)
+    {
+        prod *= i;
+    }
+    Null(number);
+    Console.Write(prod);
+    return prod;
+}
+
+Value(EnterNumber("Enter the number N: "));
+Console.ReadKey();
+
+//След задача - 1.27 видоса.
