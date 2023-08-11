a4b8775e4ed8ef174be4455dc58bb749ced20754
DemchukDS <demchuk.btg@gmail.com>
2023-08-11 11:15:36 +0300

    update

---
+++ CSharp_Seminar004/C#Seminar004_task002_NumbersOfDigit/Program.cs
@@ -0,0 +1,45 @@
+﻿// Напишите программу, укоторая принимает на вход число и выдает количество цифр в числе.
+
+using System.Diagnostics.CodeAnalysis;
+
+Console.Clear();
+
+void Null(int number)
+{
+    if (number == 0)
+    {
+        Console.WriteLine("In the entered number - 1 digit!");
+    }
+    else
+    {
+        int sum = 0;
+        while (number > 0)
+        {
+            number /= 10;
+            sum++;
+        }
+        Console.WriteLine($"In the entered number - {sum} digits!");
+    }
+}
+int Msg(string message)
+{
+    Console.WriteLine(message);
+    int msg = Convert.ToInt32(Console.ReadLine());
+    return msg;
+}
+int NumberOfDigit(int number)
+{
+    if (number < 0)
+    {
+        Console.Write("Try again! ");
+        NumberOfDigit(Msg("Enter the random number: "));
+    }
+    else
+    {
+        Null(number);
+    }
+    return number;
+}
+
+NumberOfDigit(Msg("Enter the random number: "));
+Console.ReadKey();
