6ad8fbd91af76718196584a1ceeb4f0868951a3e
DemchukDS <demchuk.btg@gmail.com>
2023-08-25 12:03:46 +0300

    HW 4, Ex 1.

---
+++ CHarp_HW004/C#HW004_task001_NaturalDegree/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4-> 16
+
+Console.Clear();
+
+int EnterNumber(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int Volue(int numberA, int numberB)
+{
+    int volue = numberA;
+    for (int i = 1; i < numberB; i++)
+    {
+        volue = volue * numberA;
+    }
+    return volue;
+}
+
+int numberA = EnterNumber($"Enter the first number: ");
+int numberB = EnterNumber($"Enter the second number: ");
+
+Console.WriteLine($"The first number {numberA} to the power of the second number {numberB} is {Volue(numberA, numberB)}!");
+
+Console.ReadKey();
