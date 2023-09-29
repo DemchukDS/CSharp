bfa91d44a22d225a29013784450c4bb2bfe47c0a
DemchukDS <demchuk.btg@gmail.com>
2023-09-29 17:08:06 +0300

    update

---
+++ CSharp_Seminar006/C#Seminar006_task001_SwaappingEllements/Program.cs
@@ -0,0 +1,35 @@
+﻿// Перевернет одномерный массив (последний эллемент на первый, а первый на последний и т.д.)
+// [1, 2, 3, 4, 5] -> [5, 4, 3, 2, 1]
+
+Console.Clear();
+
+Console.Write("Enter the quantity of elements of array: ");
+int quantityOfElements = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Program generate a new array:");
+
+int[] GenerateArray(int number)
+{
+    int[] array = new int[number];
+    for (int i = 0; i < number; i++)
+    {
+        array[i] = new Random().Next();
+    }
+    return array;
+}
+
+void SwapElementsOfArray(int[] array)
+{
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        int newElement = array[array.Length - i - 1];
+        array[array.Length - i - 1] = array[i];
+        array[i] = newElement;
+    }
+}
+
+int[] newArray = GenerateArray(quantityOfElements);
+Console.WriteLine(String.Join(", ", newArray)); //Вывод массива, чтобы не делать лишних циклов.
+SwapElementsOfArray(newArray);
+Console.WriteLine(String.Join(", ", newArray));
+Console.ReadKey();
---
+++ CSharp_Seminar006/C#Seminar006_task002_Triangle/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с такими сторонами.
+// Теорема о неравенстве треугольника: каждая сторона треугольника, меньше суммы двух других сторон.
+
+
+Console.Clear();
+
+double EnterVariable(string message)
+{
+    Console.Write(message);
+    double number = Convert.ToDouble(Console.ReadLine());
+    return number;
+}
+
+void CheckExistenceOfTriangle(double a, double b, double c)
+{
+    if (a < b + c && b < a + c && c < a + b)
+    {
+        Console.WriteLine("Треугольник с указанными сторонами может существовать!");
+    }
+    else
+    {
+        Console.WriteLine("Треугольник с указанными сторонами нереален!");
+    }
+}
+
+double firstSide = EnterVariable("Введите первую потенциальную длину треугольника: ");
+double secondSide = EnterVariable("Введите вторую потенциальную длину треугольника: ");
+double thirdSide = EnterVariable("Введите третью потенциальную длину треугольника: ");
+
+CheckExistenceOfTriangle(firstSide, secondSide, thirdSide);
+
+Console.ReadKey();
