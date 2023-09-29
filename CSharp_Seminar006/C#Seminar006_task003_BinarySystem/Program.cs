19249f2939d2b5b7fe1aaf1ba07c490c50d4fb36
DemchukDS <demchuk.btg@gmail.com>
2023-09-29 17:07:44 +0300

    Sem6, Ex 1-3, Part 1, mouvie time 30.19

---
+++ CSharp_Seminar006/C#Seminar006_task003_BinarySystem/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+Console.Clear();
+
+int EnterVariable(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+void ConvertNumberToBynarySystem(int number)
+{
+    string result = "";
+    for (int i = 0; i < number; i++)
+    {
+        if (number % 2 != 0)
+        {
+            result = result + 1;
+            number = number / 2;
+        }
+        else
+        {
+            result = result + 0;
+            number = number / 2;
+        }
+        i--;
+    }
+    Console.WriteLine(result);
+}
+
+int number = EnterVariable("Введите любое число в десятичной системе измерения: ");
+Console.Write("Указанное число в бинарной системе исчесления принимает вид: ");
+ConvertNumberToBynarySystem(number);
+
+Console.ReadKey ();
