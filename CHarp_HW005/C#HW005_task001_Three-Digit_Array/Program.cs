d7dafa55883c26eac5b29323dfe70bb555f9c2c6
DemchukDS <demchuk.btg@gmail.com>
2023-09-29 12:35:53 +0300

    HW5, Ex1,2

---
+++ CHarp_HW005/C#HW005_task001_Three-Digit_Array/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.Clear();
+
+Console.Write("Enter the quantity of elements of array: ");
+int quantityOfElements = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Program generate a new array:");
+
+int CalculateNumberOfEvenDigitsInRandomArray(int number)
+{
+    int numberOfEvenDigitsInRandomArray = 0;
+    int[] newRandomArray = new int[number];
+    Random rand = new Random();
+    for (int i = 0; i < newRandomArray.Length; i++)
+    {
+        newRandomArray[i] = rand.Next(99, 1000);
+        if (newRandomArray[i] % 2 == 0)
+        {
+            numberOfEvenDigitsInRandomArray++;
+        }
+        Console.Write($"{newRandomArray[i]} ");
+    }
+    
+    Console.WriteLine($"\n\nThe quantity of even numbers in array is {numberOfEvenDigitsInRandomArray}");
+    return numberOfEvenDigitsInRandomArray;
+}
+
+CalculateNumberOfEvenDigitsInRandomArray(quantityOfElements);
+
+Console.ReadKey();
---
+++ CHarp_HW005/C#HW005_task002_SumOfElementsOnOddPositions/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6]-> 0
+
+Console.Clear();
+
+Console.Write("Enter the quantity of elements of array: ");
+int quantityOfElements = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Program generate a new array:");
+
+int SumElementsOnOddPositions(int number)
+{
+    int sumOfElements = 0;
+    int[] newRandomArray = new int[number];
+    Random rand = new Random();
+    for (int i = 1; i < newRandomArray.Length; i++)
+    {
+        newRandomArray[i] = rand.Next(0, 100);
+        if (i % 2 != 0)
+        {
+            sumOfElements += newRandomArray[i];
+        }
+        Console.Write($"{newRandomArray[i]} ");
+    }
+
+    Console.WriteLine($"\n\nThe sum of elements on odd position is {sumOfElements}");
+    return sumOfElements;
+}
+
+SumElementsOnOddPositions(quantityOfElements);
+
+Console.ReadKey();
