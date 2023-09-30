46d0ec7eb0ce1ce48a56ebf29239e3b206b9dfde
DemchukDS <demchuk.btg@gmail.com>
2023-09-30 10:27:43 +0300

    Sem6, Ex5

---
+++ CSharp_Seminar006/C#Seminar006_task005_CopyArray/Program.cs
@@ -0,0 +1,43 @@
+﻿//Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+Console.Clear();
+
+int EnterVariable(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int[] CreateRandomArray(int number)
+{
+    int[] array = new int[number];
+    for (int i = 0; i < number; i++)
+    {
+        array[i] = new Random().Next(-20, 20);
+    }
+    return array;
+}
+
+int[] CopyElementsOfArrayInNewArray(int[] array)
+{
+    int[] newCopyratedArray = new int[array.Length];
+    for (int i = 0;i < array.Length;i++)
+    {
+        newCopyratedArray[i] = array[i];
+    }
+    return newCopyratedArray;
+}
+
+
+
+int quantityOfElements = EnterVariable("Enter the quantity of elements in array: ");
+Console.WriteLine("The program generated an array: ");
+int[] randomArray = CreateRandomArray(quantityOfElements);
+Console.WriteLine(String.Join(", ", randomArray));
+Console.WriteLine("The array would copyrated. Result is: ");
+int[] newRandomArray = CopyElementsOfArrayInNewArray(randomArray);
+Console.WriteLine(String.Join(", ",newRandomArray));
+
+
+Console.ReadKey();
