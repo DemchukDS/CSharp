244362f67ba672b3e81e55effd65f0a6f3fa530f
DemchukDS <demchuk.btg@gmail.com>
2023-10-05 22:17:14 +0300

    Sem7, task3

---
+++ CHarp_HW006/Program.cs
@@ -0,0 +1,118 @@
+﻿Console.Clear();
+
+int coefficient = 0;
+int constant = 1;
+int x_coordinate = 0;
+int y_coordinate = 1;
+int line_1 = 1;
+int line_2 = 2;
+
+int EnterIntNumberFromConsole(string message) //Внесение целых чисел из консоли
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+int[] ImputArray(int arraySize) // Заполнение массива из консоли
+{
+    int[] array = new int[arraySize];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = EnterIntNumberFromConsole($"Enter the {i + 1} element of array: ");
+    }
+    return array;
+}
+int CountPositiveNumbers(int[] array) // Поиск количества значений, которые больше 0 в массиве
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+//-----------------------------------------------------------------------------------------------------------------------------
+double EnterFractNumberFromConsole(string message) // Внесение дробных чисел из консоли
+{
+    Console.Write(message);
+    double number = Convert.ToDouble(Console.ReadLine());
+    return number;
+}
+double[] InputLineData(int numberOfLine) // Создание массива, в котором вводим характеристики уравнения, coefficient = k.., constant = b..
+{
+    double[] lineData = new double[2];
+    lineData[coefficient] = EnterFractNumberFromConsole($"Введите коэфициэнт для {numberOfLine} прямой: ");
+    lineData[constant] = EnterFractNumberFromConsole($"Введите константу для {numberOfLine} прямой: ");
+    return lineData;
+}
+double[] FindCoordinate(double[] lineData1, double[] lineData2) //поиск координат точки пересечения двух прямых в плоскости
+{
+    double[] coord = new double[2];
+    coord[x_coordinate] = (lineData1[constant] - lineData2[constant] / lineData2[coefficient] - lineData1[coefficient]);
+    coord[y_coordinate] = (lineData1[coefficient] * coord[x_coordinate] + lineData1[constant]);
+    return coord;
+}
+bool ValidateLines(double[] lineData1, double[] lineData2) // проверка на параллельность двух пнямых и на их совпадение(наложение друг на друга)
+{
+    if (lineData1[coefficient] == lineData2[coefficient])
+    {
+        if (lineData1[constant] == lineData2[constant])
+        {
+            Console.WriteLine("Прямые совпадают");
+            return false;
+        }
+        else
+        {
+            Console.WriteLine("Прямые параллельны");
+            return false;
+        }
+    }
+    return true;
+}
+
+int number = EnterIntNumberFromConsole($"Выберите номер задачи, которую необходимо решить, либо 0, чтобы посмотреть условия задач: ");
+
+switch (number)
+{
+    case 0: // Оглавление
+        Console.WriteLine("Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.");
+        Console.WriteLine("Задача 2: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;");
+        Console.ReadKey();
+        break;
+    case 1: // Задача 1
+        //Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+        //0, 7, 8, -2, -2 -> 2
+        //1, -7, 567, 89, 223-> 3
+
+        int numberOfArrayElements = EnterIntNumberFromConsole("Ente number of array elements: ");
+        int[] newArray = ImputArray(numberOfArrayElements);
+        Console.Write("The entered array: ");
+        Console.WriteLine(String.Join(", ", newArray));
+        Console.WriteLine($"The quantity of positive numbers in the array is - {CountPositiveNumbers(newArray)}");
+        Console.ReadKey();
+        break;
+    case 2: // Задача 2
+        //Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+        //значения b1, k1, b2 и k2 задаются пользователем.
+        //b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+        double[] lineData1 = InputLineData(line_1);
+        double[] lineData2 = InputLineData(line_2);
+        if (ValidateLines(lineData1, lineData2))
+        {
+            double[] coord = FindCoordinate(lineData1, lineData2);
+            Console.Write($"Точка пересечения уравнений y={lineData1[coefficient]}*x+{lineData1[constant]} и y={lineData2[coefficient]}*x+{lineData2[constant]} ");
+            Console.WriteLine($"имеет координаты ({(int)coord[x_coordinate]}, {(int)coord[y_coordinate]})");
+            //double x = coord[x_coordinate];
+            //double y = coord[y_coordinate];
+        }
+        Console.ReadKey();
+        break;
+    default:
+        Console.WriteLine("Указанной Вами задачи не существует!");
+        Console.ReadKey();
+        break;
+}
---
+++ CSharp_Seminar007/C#Seminar007_task001/Program.cs
@@ -0,0 +1,94 @@
+﻿Console.Clear();
+int numberTask = EnterNumber("Enter the task number: ");
+switch (numberTask)
+{
+    case 1: // Двумерный массив размером m*n, заполненный случайными целыми числами.
+        int row = EnterNumber($"Enter the number of matrix row: ");
+        int column = EnterNumber($"Enter the number of matrix column: ");
+
+        Console.WriteLine("The program generate matrix: ");
+        FillArrayRandomNumbers(row, column); // ПОКА НЕ ВЫВОДИТСЯ
+        Console.ReadKey();
+        break;
+
+    case 2: // Задайте двумерный масив размера m, n. Каждый элемент в массиве задается по формале A = m + n.
+        FillArrayUseFormula(EnterNumber($"Enter the number of matrix row: "), EnterNumber($"Enter the number of matrix column: "));
+        Console.ReadKey();
+        break;
+
+    case 3: // Задать двумерный массив. Найти элементы, у которых оба индекса четные и заменить их на квадраты этих элементов.
+
+        int[,] array = FillArrayRandomNumbers(EnterNumber($"Enter the number of matrix row: "), EnterNumber($"Enter the number of matrix column: "));
+        Console.WriteLine("The program generate new array: ");
+        OutputTwoDimensionalArray(array);
+        SquareElementsOfArray(array);
+        Console.WriteLine("The program has updated the array: ");
+        OutputTwoDimensionalArray(array);
+        Console.ReadKey();
+        break;
+    default:
+        Console.WriteLine("Your entered number of the task doesn't exist!");
+        Console.ReadKey();
+        break;
+}
+
+int EnterNumber(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+int[,] FillArrayRandomNumbers(int row, int column) // row - ряд, column - столбец
+{
+    int[,] array = new int[row, column];
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return array;
+}
+void FillArrayUseFormula(int row, int column)
+{
+    int[,] array = new int[row, column];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = i + j;
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+bool CheckForEven(int number)
+{
+    return number % 2 == 0;
+}
+void OutputTwoDimensionalArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void SquareElementsOfArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (CheckForEven(i) && CheckForEven(j))
+            {
+                array[i, j] = (int)Math.Pow(array[i, j], 2);
+            }
+        }
+    }
+}
