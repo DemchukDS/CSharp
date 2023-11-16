b1e064a433ae94da0474f2fc10a51329906c7ab6
DemchukDS <demchuk.btg@gmail.com>
2023-11-16 16:33:27 +0300

    Sem8

---
+++ CHarp_HW007/Program.cs
@@ -0,0 +1,128 @@
+﻿while (true)
+{
+    Console.Clear();
+    int numberTask = EnterNumber("Enter the number of the task to program output the task! You can press 0 to see the conditions of the task! You can pres 666 so that program stoped! \nPress number: ");
+    switch (numberTask)
+    {
+        case 0: // Оглавление
+
+            Console.ReadKey();
+            break;
+        case 1: //Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+            double[,] matrixWithRealNumbers = FillArrayWithRandomRealNumbers(EnterNumber($"Enter the number of matrix row: "), EnterNumber($"Enter the number of matrix column: "));
+            Console.WriteLine("\nThe program generate a new array: \n");
+            OutputTwoDimentionalRealNumberArray(matrixWithRealNumbers);
+            Console.ReadKey();
+            break;
+        case 2: //Напишите программу, которая на вход принимает значение элемента в двумерном массиве, и возвращает позицию этого элемента или же указание, что такого элемента нет.
+            int[,] matrixWithNormalNumbers = FillArrayWithRandomNormalNumbers(EnterNumber($"Enter the number of matrix row: "), EnterNumber($"Enter the number of matrix column: "));
+            Console.WriteLine("\nThe program generate a new array: \n");
+            OutputTwoDimentionalNormalNumberArray(matrixWithNormalNumbers);
+            int elementValue = EnterNumber("\nEnter the element value so that programm can check if such an element in the matrix: ");
+            FindArrayElementPositionNumber(matrixWithNormalNumbers, elementValue);
+            Console.ReadKey();
+            break;
+        case 3: //Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+            int row = EnterNumber($"Enter the number of matrix row: ");
+            int column = EnterNumber($"Enter the number of matrix column: ");
+            int[,] matrixTask3 = FillArrayWithRandomNormalNumbers(row, column);
+            Console.WriteLine("\nThe program generate a new array: \n");
+            OutputTwoDimentionalNormalNumberArray(matrixTask3);
+            CalculateAverageOfArrayColumns(matrixTask3, column);
+            Console.ReadKey();
+            break;
+        case 666: // Выход из программы
+            return;
+        default: // Если указываетя несуществующая задача.
+            Console.WriteLine("Your entered number of the task doesn't exist!");
+            Console.ReadKey();
+            break;
+    }
+}
+
+int EnterNumber(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+double[,] FillArrayWithRandomRealNumbers(int row,  int column)
+{
+    double[,] array = new double[row, column];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Math.Round(new Random().Next(-9, 10) * new Random().NextDouble(), 2);
+        }
+    }
+    return array;
+}
+int[,] FillArrayWithRandomNormalNumbers(int row, int column)
+{
+    int[,] array = new int[row, column];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return array;
+}
+void OutputTwoDimentionalRealNumberArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void OutputTwoDimentionalNormalNumberArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void FindArrayElementPositionNumber(int[,] array, int number)
+{
+    bool hasNumber = false;
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] == number)
+            {
+                Console.WriteLine($"Your entered number at the position [{i},{j}] in the matrix!");
+                hasNumber = true;
+                break;
+            }
+        }
+    }
+    if (!hasNumber)
+    {
+        Console.WriteLine("The entered number is absend!");
+    }
+}
+void CalculateAverageOfArrayColumns(int[,] array, int quantityOfColumns)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        double average = 0;
+        double sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[j, i];
+        }
+        average = sum / quantityOfColumns;
+        Console.WriteLine($"The average numbers in {i+1} column is {average}!");
+    }
+}
---
+++ CSharp_Seminar008/Program.cs
@@ -0,0 +1,119 @@
+﻿while (true)
+{
+    Console.Clear();
+    int numberTask = EnterNumber("Enter the number of the task to program output the task! You can press 0 to see the conditions of the task! You can pres 666 so that program stoped! \nPress number: ");
+    switch (numberTask)
+    {
+        case 0: // Оглавление
+            Console.WriteLine("Below is a list of tasks: ");
+            Console.WriteLine("Task 1. Enter the row and column of the matrix, so that the program fils this matrix with random elements.");
+            Console.WriteLine("Task 2. Enter the row = M and column = N of the matrix! All enements of the matrix filling to the formula A = M + N.");
+            Console.WriteLine("Task 3. Enter the row and column of the matrix! If both index of the matrix eleements even, this elements is squared.");
+
+            Console.ReadKey();
+            break;
+        case 1: // Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+            int row = EnterNumber($"Enter the number of matrix row: ");
+            int column = EnterNumber($"Enter the number of matrix column: ");
+
+            Console.WriteLine("The program generate matrix: ");
+            int[,] originalArray = FillArrayRandomNumbers(row, column);
+            OutputTwoDimensionalArray(originalArray);
+            Console.WriteLine("\nAfter replase first and last rows matrix is: ");
+            ReplaseMatrixFirstAndLastRows(originalArray);
+            Console.ReadKey();
+            break;
+        case 2: // Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+            int rowTaskTwo = EnterNumber($"Enter the number of matrix row: ");
+            int columnTaskTwo = EnterNumber($"Enter the number of matrix column: ");
+
+            Console.WriteLine("The program generate matrix: ");
+            int[,] secondArray = FillArrayRandomNumbers(rowTaskTwo, columnTaskTwo);
+            OutputTwoDimensionalArray(secondArray);
+            if (rowTaskTwo != columnTaskTwo)
+            {
+                Console.WriteLine("The rows and columns in the generated matrix cannot swapped!");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("\nAfter replase matrix rows and columns array is: ");
+                ReplaseMatrixRowsAndColumns(secondArray);
+                Console.ReadKey();
+
+            }
+            break;
+        case 666: // Выход из программы
+            return;
+        default: // Если указываетя несуществующая задача.
+            Console.WriteLine("Your entered number of the task doesn't exist!");
+            Console.ReadKey();
+            break;
+    }
+}
+
+int EnterNumber(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+int[,] FillArrayRandomNumbers(int row, int column) // row - ряд, column - столбец
+{
+    int[,] array = new int[row, column];
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return array;
+}
+void OutputTwoDimensionalArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void ReplaseMatrixFirstAndLastRows(int[,] array)
+{
+    int[] tempArray = new int[array.GetLength(1)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i == 0)
+            {
+                tempArray[j] = array[i, j];
+                array[i, j] = array[array.GetLength(0) - 1, j];
+                array[array.GetLength(0) - 1, j] = tempArray[j];
+            }
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void ReplaseMatrixRowsAndColumns(int[,] array)
+{
+    int[,] tempArray = new int[array.GetLength(0), array.GetLength(1)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            tempArray[i, j] = array[j, i];
+            //temp = array[i, j];
+            //array[i, j] = array[j, i];
+            //array[j, i] = temp;
+            //Console.Write($"{array[i, j]} ");
+            Console.Write($"{tempArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
